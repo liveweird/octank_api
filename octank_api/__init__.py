9ab34628fd9acc4a3de4a7d7b30e897f06879b6a
Sebastian Gebski <sebastian@gebski.net>
2021-03-06 21:53:50 +0100

    API returns a list of shows

--- octank_api/__init__.py
+++ octank_api/__init__.py
@@ -7,30 +7,30 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhos
 
 db = SQLAlchemy(app)
 
-# define ORM structures
-class user(db.Model):
-    user = db.Column(db.String(64), primary_key=True)
-    gender = db.Column(db.Integer, nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    state = db.Column(db.String(16), nullable=False)
-
-    def __init__(self, user, gender, age, state):
-        self.user = user
-        self.gender = gender
-        self.age = age
-        self.state = state
-
-    def serialize(self):
-        return {
-            'user': self.user,
-            'gender': self.gender,
-            'age': self.age,
-            'state': self.state
-        }
-
 # read all users to impersonate
 @app.route('/api/users', methods=['GET'])
 def read_users():
+
+    class user(db.Model):
+        user = db.Column(db.String(64), primary_key=True)
+        gender = db.Column(db.Integer, nullable=False)
+        age = db.Column(db.Integer, nullable=False)
+        state = db.Column(db.String(16), nullable=False)
+
+        def __init__(self, user, gender, age, state):
+            self.user = user
+            self.gender = gender
+            self.age = age
+            self.state = state
+
+        def serialize(self):
+            return {
+                'user': self.user,
+                'gender': self.gender,
+                'age': self.age,
+                'state': self.state
+            }
+
     return jsonify({
         'users': [user.serialize() for user in user.query.all()]
     }), 200
@@ -38,8 +38,28 @@ def read_users():
 # get all shows to watch
 @app.route('/api/shows', methods=['GET'])
 def read_shows():
+    class show(db.Model):
+        show = db.Column(db.String(64), primary_key=True)
+        origin = db.Column(db.String(32), nullable=False)
+        genre = db.Column(db.Integer, nullable=False)
+        pgrating = db.Column(db.Integer, nullable=False)
+
+        def __init__(self, show, origin, genre, pgrating):
+            self.show = show
+            self.origin = origin
+            self.genre = genre
+            self.pgrating = pgrating
+
+        def serialize(self):
+            return {
+                'show': self.show,
+                'origin': self.origin,
+                'genre': self.genre,
+                'pgrating': self.pgrating
+            }
+
     return jsonify({
-        'shows': []
+        'shows': [show.serialize() for show in show.query.all()]
     }), 200
 
 @app.route('/api/watching', methods=['POST'])
