e19ffee21f0ec59fa1f91ccde5133419ed623ecb
Sebastian Gebski <sebastian@gebski.net>
2021-03-06 21:41:54 +0100

    API returns a list of users

--- octank_api/__init__.py
+++ octank_api/__init__.py
@@ -1,14 +1,39 @@
 import json
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/pg_octank_api'
+
+db = SQLAlchemy(app)
+
+# define ORM structures
+class user(db.Model):
+    user = db.Column(db.String(64), primary_key=True)
+    gender = db.Column(db.Integer, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    state = db.Column(db.String(16), nullable=False)
+
+    def __init__(self, user, gender, age, state):
+        self.user = user
+        self.gender = gender
+        self.age = age
+        self.state = state
+
+    def serialize(self):
+        return {
+            'user': self.user,
+            'gender': self.gender,
+            'age': self.age,
+            'state': self.state
+        }
 
 # read all users to impersonate
 @app.route('/api/users', methods=['GET'])
 def read_users():
     return jsonify({
-        'users': []
-    }), 209
+        'users': [user.serialize() for user in user.query.all()]
+    }), 200
 
 # get all shows to watch
 @app.route('/api/shows', methods=['GET'])
