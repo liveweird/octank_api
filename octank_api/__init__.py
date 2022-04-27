0505c7d4fcba29681765da6412303c4c9b36a52a
Sebastian Gebski <sebastian@gebski.net>
2022-04-27 18:07:27 +0200

    Adding OpenTelemetry instrumentation

--- octank_api/__init__.py
+++ octank_api/__init__.py
@@ -10,7 +10,24 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
 
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestInstrumentor
+from opentelemetry.sdk.trace impor TraceProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter
+)
+
+trace.set_trace_provider(TraceProvider())
+trace.get_trace_provider().add_span_processor(
+    BatchSpanProcessor(ConsoleSpanExporter())
+)
+
 app = Flask(__name__, instance_relative_config=True)
+FlaskInstrumentor().instrument_app(app)
+RequestInstrumentor().instrument()
+tracer = trace.get_tracer(__name__)
 
 endpoint = environ.get('OCTANK_AURORA_ENDPOINT')
 dbname = environ.get('OCTANK_AURORA_DBNAME')
@@ -41,7 +58,8 @@ class EventType(Enum):
 # health check
 @app.route('/', methods=['GET'])
 def health_check():
-    return jsonify({}), 200
+    with tracer.start_as_current_span("health-check"):
+        return jsonify({}), 200
 
 
 class user(db.Model):
@@ -51,41 +69,45 @@ class user(db.Model):
     state = db.Column(db.String(16), nullable=False)
 
     def __init__(self, user, gender, age, state):
-        self.user = user
-        self.gender = gender
-        self.age = age
-        self.state = state
+        with tracer.start_as_current_span("user-init"):
+            self.user = user
+            self.gender = gender
+            self.age = age
+            self.state = state
 
     def serialize(self):
-        return {
-            'user': self.user,
-            'gender': self.gender,
-            'age': self.age,
-            'state': self.state
-        }
+        with tracer.start_as_current_span("user-serialize"):
+            return {
+                'user': self.user,
+                'gender': self.gender,
+                'age': self.age,
+                'state': self.state
+            }
 
 
 # fake the users table query (for local development w/o Postgres)
 def query_users():
-    return [
-        user('Ana-0-20-MZ', 0, 20, 'MZ'),
-        user('Bob-1-30-SW', 1, 30, 'SW'),
-        user('Chloe-0-40-MP', 0, 40, 'MP'),
-        user('Don-1-50-WP', 1, 50, 'WP'),
-        user('Eve-0-25-SW', 0, 25, 'SW'),
-        user('Flynn-1-35-MP', 1, 35, 'MP'),
-        user('Gina-0-45-MZ', 0, 45, 'MZ'),
-        user('Hugo-1-15-MZ', 1, 15, 'MZ')
-    ]
+    with tracer.start_as_current_span("query-hardcoded-users"):
+        return [
+            user('Ana-0-20-MZ', 0, 20, 'MZ'),
+            user('Bob-1-30-SW', 1, 30, 'SW'),
+            user('Chloe-0-40-MP', 0, 40, 'MP'),
+            user('Don-1-50-WP', 1, 50, 'WP'),
+            user('Eve-0-25-SW', 0, 25, 'SW'),
+            user('Flynn-1-35-MP', 1, 35, 'MP'),
+            user('Gina-0-45-MZ', 0, 45, 'MZ'),
+            user('Hugo-1-15-MZ', 1, 15, 'MZ')
+        ]
 
 
 # read all users to impersonate
 @app.route('/api/users', methods=['GET'])
 def read_users():
-    return jsonify({
-        'users': [user.serialize() for user in user.query.all()]
-        # 'users': [user.serialize() for user in query_users()]
-    }), 200
+    with tracer.start_as_current_span("read-users"):
+        return jsonify({
+            'users': [user.serialize() for user in user.query.all()]
+            # 'users': [user.serialize() for user in query_users()]
+        }), 200
 
 
 class show(db.Model):
@@ -95,129 +117,135 @@ class show(db.Model):
     pgrating = db.Column(db.Integer, nullable=False)
 
     def __init__(self, show, origin, genre, pgrating):
-        self.show = show
-        self.origin = origin
-        self.genre = genre
-        self.pgrating = pgrating
+        with tracer.start_as_current_span("show-init"):
+            self.show = show
+            self.origin = origin
+            self.genre = genre
+            self.pgrating = pgrating
 
     def serialize(self):
-        return {
-            'show': self.show,
-            'origin': self.origin,
-            'genre': self.genre,
-            'pgrating': self.pgrating,
-            'recommended': random.choice([True, False])
-        }
+        with tracer.start_as_current_span("show-serialize"):
+            return {
+                'show': self.show,
+                'origin': self.origin,
+                'genre': self.genre,
+                'pgrating': self.pgrating,
+                'recommended': random.choice([True, False])
+            }
 
 
 # fake the shows table query (for local development w/o Postgres)
 def query_shows():
-    return [
-        show('Avatar-AU-1-1', 'Austria', 1, '1', False),
-        show('Breaking Bad-BR-2-2', 'Brasil', 2, '2', True),
-        show('Chernobyl-CH-3-3', 'Chile', 3, '3', True),
-        show('Dracula-DE-1-4', 'Denmark', 1, '4', False),
-        show('Exorcist-AU-1-5', 'Austria', 1, '5', False),
-        show('Family Guy-BR-2-1', 'Brasil', 2, '1', True),
-        show('Goonies-CH-2-1', 'Chile', 2, '1', False),
-        show('Home Alone-DE-1-2', 'Denmark', 1, '2', False)
-    ]
+    with tracer.start_as_current_span("query-hardcoded-shows"):
+        return [
+            show('Avatar-AU-1-1', 'Austria', 1, '1', False),
+            show('Breaking Bad-BR-2-2', 'Brasil', 2, '2', True),
+            show('Chernobyl-CH-3-3', 'Chile', 3, '3', True),
+            show('Dracula-DE-1-4', 'Denmark', 1, '4', False),
+            show('Exorcist-AU-1-5', 'Austria', 1, '5', False),
+            show('Family Guy-BR-2-1', 'Brasil', 2, '1', True),
+            show('Goonies-CH-2-1', 'Chile', 2, '1', False),
+            show('Home Alone-DE-1-2', 'Denmark', 1, '2', False)
+        ]
 
 
 # get all shows to watch
 @app.route('/api/shows', methods=['GET'])
 def read_shows():
-    return jsonify({
-        'shows': [show.serialize() for show in show.query.all()]
-        # 'shows': [show.serialize() for show in query_shows()]
-    }), 200
+    with tracer.start_as_current_span("read-shows"):
+        return jsonify({
+            'shows': [show.serialize() for show in show.query.all()]
+            # 'shows': [show.serialize() for show in query_shows()]
+        }), 200
 
 
 @app.route('/api/events', methods=['POST'])
 def event_sink():
-    # obligatory
-    device_param = request.form.get('device', type=str)
-    user_param = request.form.get('user', type=str)
-    event_param = request.form.get('event', type=str)
+    with tracer.start_as_current_span("event-sink") as span:
+        # obligatory
+        device_param = request.form.get('device', type=str)
+        user_param = request.form.get('user', type=str)
+        event_param = request.form.get('event', type=str)
+        span.set_attribute("device", device_param)
 
-    # optional
-    show_param = request.form.get('show', default='', type=str)
-    recommended_param = request.form.get('recommended', default=0, type=int)
-    tstamp = datetime.now().isoformat()
+        # optional
+        show_param = request.form.get('show', default='', type=str)
+        recommended_param = request.form.get('recommended', default=0, type=int)
+        tstamp = datetime.now().isoformat()
 
-    # event builder
-    builder = {
-        EventType.LOGIN.value: lambda: {
-            'eventType': EventType.LOGIN.value,
-            'deviceId': device_param,
-            'userId': user_param,
-            'tstamp': tstamp
-        },
-        EventType.LOGIN_FAILED.value: lambda: {
-            'eventType': EventType.LOGIN_FAILED.value,
-            'deviceId': device_param,
-            'userId': user_param,
-            'tstamp': tstamp
-        },
-        EventType.LOGOUT.value: lambda: {
-            'eventType': EventType.LOGOUT.value,
-            'deviceId': device_param,
-            'userId': user_param,
-            'tstamp': tstamp
-        },
-        EventType.DETAILS.value: lambda: {
-            'eventType': EventType.DETAILS.value,
-            'deviceId': device_param,
-            'userId': user_param,
-            'showId': show_param,
-            'recommended': recommended_param,
-            'tstamp': tstamp
-        },
-        EventType.TRAILER.value: lambda: {
-            'eventType': EventType.TRAILER.value,
-            'deviceId': device_param,
-            'userId': user_param,
-            'showId': show_param,
-            'recommended': recommended_param,
-            'tstamp': tstamp
-        },
-        EventType.WATCHING.value: lambda: {
-            'eventType': EventType.WATCHING.value,
-            'deviceId': device_param,
-            'userId': user_param,
-            'showId': show_param,
-            'recommended': recommended_param,
-            'tstamp': tstamp
-        },
-        EventType.FINISHED.value: lambda: {
-            'eventType': EventType.FINISHED.value,
-            'deviceId': device_param,
-            'userId': user_param,
-            'showId': show_param,
-            'recommended': recommended_param,
-            'tstamp': tstamp
+        # event builder
+        builder = {
+            EventType.LOGIN.value: lambda: {
+                'eventType': EventType.LOGIN.value,
+                'deviceId': device_param,
+                'userId': user_param,
+                'tstamp': tstamp
+            },
+            EventType.LOGIN_FAILED.value: lambda: {
+                'eventType': EventType.LOGIN_FAILED.value,
+                'deviceId': device_param,
+                'userId': user_param,
+                'tstamp': tstamp
+            },
+            EventType.LOGOUT.value: lambda: {
+                'eventType': EventType.LOGOUT.value,
+                'deviceId': device_param,
+                'userId': user_param,
+                'tstamp': tstamp
+            },
+            EventType.DETAILS.value: lambda: {
+                'eventType': EventType.DETAILS.value,
+                'deviceId': device_param,
+                'userId': user_param,
+                'showId': show_param,
+                'recommended': recommended_param,
+                'tstamp': tstamp
+            },
+            EventType.TRAILER.value: lambda: {
+                'eventType': EventType.TRAILER.value,
+                'deviceId': device_param,
+                'userId': user_param,
+                'showId': show_param,
+                'recommended': recommended_param,
+                'tstamp': tstamp
+            },
+            EventType.WATCHING.value: lambda: {
+                'eventType': EventType.WATCHING.value,
+                'deviceId': device_param,
+                'userId': user_param,
+                'showId': show_param,
+                'recommended': recommended_param,
+                'tstamp': tstamp
+            },
+            EventType.FINISHED.value: lambda: {
+                'eventType': EventType.FINISHED.value,
+                'deviceId': device_param,
+                'userId': user_param,
+                'showId': show_param,
+                'recommended': recommended_param,
+                'tstamp': tstamp
+            }
         }
-    }
 
-    # build event
-    stream_event_builder = builder.get(int(event_param), lambda: {})
-    json_load = json.dumps(stream_event_builder())
-    print(json_load)
+        # build event
+        stream_event_builder = builder.get(int(event_param), lambda: {})
+        json_load = json.dumps(stream_event_builder())
+        print(json_load)
 
-    # send event
-    # response = kinesis.put_record(
-    #     StreamName="octank-kinesis-data-stream",
-    #     Data=json_load,
-    #     PartitionKey=user_param
-    # )
+        # send event
+        # response = kinesis.put_record(
+        #     StreamName="octank-kinesis-data-stream",
+        #     Data=json_load,
+        #     PartitionKey=user_param
+        # )
 
-    # analyze response
-    # ordinal = response["SequenceNumber"]
-    ordinal = 123
+        # analyze response
+        # ordinal = response["SequenceNumber"]
+        ordinal = 123
 
-    return jsonify({
-        'seq': ordinal
-    }), 201
+        return jsonify({
+            'seq': ordinal
+        }), 201
 
 
 if __name__ == '__main__':
